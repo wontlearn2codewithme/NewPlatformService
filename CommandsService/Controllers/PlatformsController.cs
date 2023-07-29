f500b3d46402603942900cf7b59ae5e36bf85dbc
wontlearn2codewithme <wontlear2codewithme@gmail.com>
2023-07-29 15:34:21 +0200

    added command service 3:41:50

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CommandsService.Controllers
+{
+    [Route("api/commands/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound post # command service");
+
+            return Ok("Inbound test of from platforms controller");
+        }
+    }
+}
+
